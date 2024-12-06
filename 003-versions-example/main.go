b7ce88a56f605ed2f7f0ceb96d7890fd7a425384
Riya Chaubey <riya.chaubey@ericom.com>
2024-12-07 00:47:54 +0530

    git tag example still

--- 003-versions-example/main.go
+++ 003-versions-example/main.go
@@ -7,9 +7,10 @@ git tag - to check if any tags exist in the repo
 steps for creating a tag
 1. make changes
 2. git add .
-3. git tag v1.0.0
-4. goit commit and push
+3. git commit and push
+4. git tag v1.0.0
 5. git tag (should/will show v1.0.0)
+6. git push tag v1.0.0
 
 git show v1.0.0 - show some info about that tag
 
