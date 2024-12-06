2165b6c7fd42f0fdd6a0b4e13ab431b4c22ab596
Riya Chaubey <riya.chaubey@ericom.com>
2024-12-07 00:41:44 +0530

    some comments for got tag 003

--- 003-versions-example/main.go
+++ 003-versions-example/main.go
@@ -4,7 +4,14 @@ import "fmt"
 
 /*
 git tag - to check if any tags exist in the repo
+steps for creating a tag
+1. make changes
+2. git add .
+3. git tag v1.0.0
+4. goit commit and push
+5. git tag (should/will show v1.0.0)
 
+git show v1.0.0 - show some info about that tag
 
 */
 
